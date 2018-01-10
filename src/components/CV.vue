84e4f932a208659d58aa5d09b9c942a1b5b083e8
Max McKenzie <max.mckenzie123@gmail.com>
2018-01-10 18:21:01 +0100

    more type changes to cv

--- src/components/CV.vue
+++ src/components/CV.vue
@@ -4,11 +4,10 @@
     <article class="cv">
       <h1>Max M<sup>c</sup>Kenzie</h1>
       <h2>Full Stack Developer, QA tester and Technical writer</h2>
-      <p>I’m a full stack developer based in Barcelona. I enjoy designing and building APIs and UIs with modern tools like ES6, Vue, React, Webpack, Nightwatch, Mocha, PostgreSQL, and GraphQL.</p>
-      <p>Over the course of my nine-year career, I have worked for agencies, SaaS providers, and publishers. Taking on the roles of technical writer, tech lead, project manager, consultant, company director, and of course, front-line pixel pusher.</p>
+      <p class="intro">Based in Barcelona. I enjoy designing and building APIs and UIs with modern tools like ES6, Vue, React, Webpack, Nightwatch, Mocha, PostgreSQL, and GraphQL.</p>
+      <p class="intro">Over the course of my nine-year career, I have worked for agencies, SaaS providers, and publishers. Taking on the roles of technical writer, tech lead, project manager, consultant, company director, and of course, front-line pixel pusher.</p>
       <div class="cv-content">
         <div class="work">
-          <h2>Work History</h2>
           <ul>
             <li v-for="role in apiData.workhistory">
               <h3>{{ role.title }}</h3>
@@ -139,11 +138,11 @@ article {
 
 .cv {
   h1 {
-    margin:0 0 1rem 0;
+    margin:0 0 0.75rem 0;
     font-size:2rem;
   }
   h2 {
-    margin:1rem 0rem 0rem 0rem;
+    margin:1rem 0rem 0.5rem 0rem;
     font-weight: 800;
     font-size:1.1rem;
   }
@@ -198,6 +197,14 @@ article {
   }
 }
 
+p.intro {
+  max-width:40rem;
+}
+
+p.intro + p.intro {
+ margin-top:0.5rem;
+}
+
 @media print {
   article {
     border:0;
@@ -208,7 +215,7 @@ article {
   }
   @page {
     size: A4;
-    margin: 5mm 5mm 5mm 5mm; /* change the margins as you want them to be. */
+    margin: 5mm; /* change the margins as you want them to be. */
   }
   h1, h2, h3, h4, h5 {
     page-break-after: avoid;
@@ -219,11 +226,6 @@ article {
   p {
     page-break-inside: avoid;
     page-break-after: avoid;
-  }
-  @page:right{
-    @bottom-right {
-      content: "Page " counter(page) " of " counter(pages);
-    }
   }
 
   .print {
